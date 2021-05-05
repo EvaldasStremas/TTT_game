ee9701d84b8652e95ed9eca8ed296bd0cb1f718c
EvaldasStremas <evaldas.stremas123@gmail.com>
2021-05-05 15:20:07 +0300

    - Seperated game logic functions - Fixed end game logic

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ NUM_ROWS = 3
 NUM_COLS = 3
 EMPTY_SYMBOL = " "
 
+
 def get_empty_grid(num_cols, num_rows, empty_symbol):
     grid = []
 
@@ -15,41 +16,120 @@ def get_empty_grid(num_cols, num_rows, empty_symbol):
         grid.append(row)
     return grid
 
+
 def print_grid(grid):
     for i in range(NUM_ROWS):
         print(grid[i])
 
-def check_scores(player, grid, points):
-    print()
-    return score_result
 
-def player_input(player_indication, grid):
-    os.system('cls' if os.name == 'nt' else 'clear')
+def enter_player_input(player_indication, grid):
+    # Checking X or O player enter input is valid integer
+    os.system('cls' if os.name == 'nt' else 'clear')  # 1
 
     print_grid(grid)
-    print()
 
-    print('Where add ', player_indication,' symbol?')
-    x_axis = int(input('In X axis (0..2): '))
-    y_axis = int(input('In Y axis (0..2): '))
+    # print('PLAYER ', player_indication)
 
+    while True:
+        # This while loop stands for checking is x and y axis points is already not used in grid
+        try:
+            while True:
+                # This while loop stands for checking valid x axis integer
+                try:
+                    print('PLAYER ', player_indication)
+                    x_axis = input('Enter integer in X axis (From 0 to 2): ')
+                    x_axis = int(x_axis)
+                    break
+                except ValueError:
+                    print("No valid integer! Please try again ...")
+                # except IndexError:
+                #     print("Value should be from 0 to 2! Please try again ...")
 
-    grid[y_axis][x_axis] = player_indication
+            while True:
+                # This while loop stands for checking valid y axis integer
+                try:
+                    print('PLAYER ', player_indication)
+                    y_axis = input('Enter integer in Y axis (From 0 to 2): ')
+                    y_axis = int(y_axis)
+                    break
+                except ValueError:
+                    print("No valid integer! Please try again ...")
+                # except IndexError:
+                #     print("Value should be from 0 to 2! Please try again ...")
+
+            if grid[y_axis][x_axis] != "X" and grid[y_axis][x_axis] != "O":
+                grid[y_axis][x_axis] = player_indication
+                break
+            else:
+                print('Window is already used in grid! Please try again ...')
+                input("Press ENTER")
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print_grid(grid)
+
+        except ValueError:
+            print()
 
     return grid
 
-# ------------------------------------MAIN-----------------------------------------
 
-points = [
-        [[0, 0], [0, 1], [0, 2]], # 1 horizontal line
-        [[1, 0], [1, 1], [1, 2]], # 2 horizontal line
-        [[2, 0], [2, 1], [2, 2]], # 3 horizontal line
-        [[0, 0], [1, 0], [2, 0]], # 1 vertical line
-        [[0, 1], [1, 1], [2, 1]], # 2 vertical line
+def check_winner(player_indication, grid):
+    # Check is player X or O is winner
+    points = [
+        [[0, 0], [0, 1], [0, 2]],  # 1 horizontal line
+        [[1, 0], [1, 1], [1, 2]],  # 2 horizontal line
+        [[2, 0], [2, 1], [2, 2]],  # 3 horizontal line
+        [[0, 0], [1, 0], [2, 0]],  # 1 vertical line
+        [[0, 1], [1, 1], [2, 1]],  # 2 vertical line
         [[0, 2], [1, 2], [2, 2]],  # 3 vertical line
-        [[2, 2], [1, 1], [0, 0]], # \ cross line
+        [[2, 2], [1, 1], [0, 0]],  # \ cross line
         [[2, 0], [1, 1], [0, 2]]  # / cross line
-        ]
+    ]
+
+    lines_counter = []
+    result_list = []
+
+    all_players_result = get_all_players_result(player_indication, grid)
+
+    for z in range(len(points)):
+        for c in range(len(points[z])):
+            lines_counter.append(grid[points[z][c][0]][points[z][c][1]])
+
+        result_counter = lines_counter.count(player_indication)
+        result_list.append(result_counter)
+        lines_counter = []
+    # print(result_list)
+    # print(all_players_result)
+    input("Press ENTER")
+
+    os.system('cls' if os.name == 'nt' else 'clear')  # 1
+
+    if 3 in result_list:
+        print('Winner is ', player_indication, ' palyer')
+        game_over = True
+        return game_over
+
+    if all_players_result == 9:
+        print('Grid is full. Game ended.')
+        game_over = True
+        return game_over
+
+    print()
+
+
+def get_all_players_result(player_indication, grid):
+    all_grid_elements_list = []
+
+    for nums in range(NUM_ROWS):
+        for cols in range(NUM_COLS):
+            all_grid_elements_list.append(grid[nums][cols])
+    x_player_results = all_grid_elements_list.count('X')
+    o_player_results = all_grid_elements_list.count('O')
+    all_grid_elements_values = x_player_results + o_player_results
+
+    return all_grid_elements_values
+
+
+# ------------------------------------MAIN-----------------------------------------
 
 def main():
     game_over = False
@@ -57,53 +137,19 @@ def main():
 
     while not game_over:
 
-        print(player_input('X', grid))
-        print(player_input('O', grid))
+        grid = enter_player_input('X', grid)
+        game_over = check_winner('X', grid)
 
-        # os.system('cls' if os.name == 'nt' else 'clear')
+        if game_over == True:
+            break
 
-        # print_grid(grid)
-        # print()
-
-        # print('First player: Where add "X" symbol?')
-        # x_value_x_axis = int(input('In X axis (0..2): '))
-        # x_value_y_axis = int(input('In Y axis (0..2): '))
-
-        # grid[x_value_y_axis][x_value_x_axis] = 'X'
-
-        # os.system('cls' if os.name == 'nt' else 'clear')
-
-        # print_grid(grid)
-        # print()
-
-        # print('Second player: Where add "O" symbol?')
-        # o_value_x_axis = int(input('In X axis (0..2): '))
-        # o_value_y_axis = int(input('In Y axis (0..2): '))
-
-        # grid[o_value_y_axis][o_value_x_axis] = 'O'
-
-        os.system('cls' if os.name == 'nt' else 'clear')
+        grid = enter_player_input('O', grid)
+        game_over = check_winner('O', grid)
 
         print_grid(grid)
 
-        lines_counter = []
-        x_list = []
-
-        for z in range(len(points)):
-            for c in range(len(points[z])):
-                lines_counter.append(grid[points[z][c][0]][points[z][c][1]])
-
-            x_counter = lines_counter.count("X")
-            x_list.append(x_counter)
-            lines_counter = []
-        print(x_list)
-
-        if 3 in x_list:
-            print('Winner is X palyer')
-            game_over = True
-
         print()
-        input("Press ENTER")
+
 
 if __name__ == '__main__':
     main()
